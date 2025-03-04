c62ea8f9429fccb1780884ac5c70c084ff036bcd
Ben Landrum <8enlandrum@gmail.com>
2025-03-03 21:01:54 -0500

    new querying in main

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,24 @@ use std::env::args;
 use std::path::Path;
 use std::time::Instant;
 
+use rand_distr::num_traits::ToPrimitive;
 use scratch::data_handling::dataset::VectorDataset;
+use scratch::data_handling::dataset_traits::Dataset;
 use scratch::data_handling::fbin::read_fbin;
+use scratch::graph::beam_search::beam_search;
+use scratch::graph::graph::{ClassicGraph, Graph};
 
 fn main() {
     let default_data_file = String::from("data/word2vec-google-news-300_50000_lowercase/base.fbin");
+    let default_query_file = String::from("data/word2vec-google-news-300_50000_lowercase/query.fbin");
+    let default_graph_file = String::from("data/word2vec-google-news-300_50000_lowercase/outputs/vamana");
 
     // presumably we'll have some command line arguments here
 
     let data_path_arg = args().nth(1).unwrap_or(default_data_file);
     let data_path = Path::new(&data_path_arg);
 
-    let start = Instant::now();
+    let mut start = Instant::now();
 
     let dataset: VectorDataset<f32> = read_fbin(&data_path);
 
@@ -24,5 +30,32 @@ fn main() {
         "read dataset in {}.{:03} seconds",
         elapsed.as_secs(),
         elapsed.subsec_millis()
+    );
+    start = Instant::now();
+
+    let graph = ClassicGraph::read(&default_graph_file).unwrap();
+
+    let elapsed = start.elapsed();
+    println!(
+        "read graph in {}.{:03} seconds",
+        elapsed.as_secs(),
+        elapsed.subsec_millis()
+    );
+    
+
+    // why bother timing reading queries
+    let queries: VectorDataset<f32> = read_fbin(&Path::new(&default_query_file));
+
+    start = Instant::now();
+
+    let results: Vec<Vec<u32>> = (0..queries.size()).map(|i| beam_search(queries.get(i), &graph, &dataset, 0, 12)).collect();
+
+    let elapsed = start.elapsed();
+    println!(
+        "ran {} queries in {}.{:03} seconds ({} QPS)",
+        queries.size(),
+        elapsed.as_secs(),
+        elapsed.subsec_millis(),
+        queries.size().to_f64().unwrap() / elapsed.as_secs_f64()
     );
 }
