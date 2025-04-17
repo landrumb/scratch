b240507a47f97399e047a27f89f78e8e14b00587
Ben Landrum <8enlandrum@gmail.com>
2025-04-17 11:44:39 -0400

    added a bunch of methods to vectorgraph

--- src/graph/vector_graph.rs
+++ src/graph/vector_graph.rs
@@ -1,23 +1,37 @@
 //! a graph implementation using a vector of vectors
 
+use std::sync::Mutex;
+
+use rayon::prelude::*;
+
 use crate::graph::IndexT;
 
 use super::{ClassicGraph, Graph, MutableGraph};
 
 pub struct VectorGraph {
     neighborhoods: Vec<Vec<IndexT>>,
+    insertion_queues: Vec<Mutex<Vec<IndexT>>>,
 }
 
 impl VectorGraph {
     /// constructs a new empty VectorGraph with the given number of nodes
     pub fn empty(n: usize) -> VectorGraph {
+        let neighborhoods: Vec<Vec<IndexT>> = vec![Vec::new(); n];
+        let insertion_queues: Vec<Mutex<Vec<IndexT>>> = (0..n)
+            .map(|_| Mutex::new(Vec::new()))
+            .collect();
+
         VectorGraph {
-            neighborhoods: vec![Vec::new(); n],
+            neighborhoods,
+            insertion_queues
         }
     }
 
     pub fn new(neighborhoods: Vec<Vec<IndexT>>) -> VectorGraph {
-        VectorGraph { neighborhoods }
+        let insertion_queues: Vec<Mutex<Vec<IndexT>>> = (0..neighborhoods.len())
+            .map(|_| Mutex::new(Vec::new()))
+            .collect();
+        VectorGraph { neighborhoods, insertion_queues }
     }
 
     /// returns the number of nodes in the graph
@@ -43,6 +57,58 @@ impl VectorGraph {
             .map(|n| n.len())
             .max()
             .unwrap_or(0)
+    }
+
+    /// pushes a new edge to the insertion queue of a node
+    pub fn queue_edge(&self, from: IndexT, to: IndexT) {
+        assert!(from < self.n() as IndexT && to < self.n() as IndexT);
+        let mut queue = self.insertion_queues[from as usize].lock().unwrap();
+        queue.push(to);
+    }
+    /// pushes a slice of new edges to the insertion queue of a node
+    pub fn bulk_queue(&self, from: IndexT, to: &[IndexT]) {
+        assert!(from < self.n() as IndexT);
+        let mut queue = self.insertion_queues[from as usize].lock().unwrap();
+        queue.extend_from_slice(to);
+    }
+
+    /// For each queue that is not empty and is small enough that adding the edges does not violate the
+    /// degree bound, we add the edges to the graph and clear the queue. Otherwise, we do nothing.
+    pub fn preprocess_queues(&mut self, degree_bound: usize) {
+        self.insertion_queues
+            .par_iter_mut()
+            .zip(self.neighborhoods.par_iter_mut()) 
+            .for_each(|(queue, neighborhood)| {
+                let mut queue = queue.lock().unwrap();
+                if queue.is_empty() {
+                    return;
+                }
+                if queue.len() + neighborhood.len() > degree_bound {
+                    return;
+                }
+                neighborhood.extend(queue.iter());
+                queue.clear();
+            });
+    }
+
+    /// returns a vector of (index, queue) pairs for all non-empty queues
+    /// and clears the queues
+    pub fn get_queued_edges(&mut self) -> Vec<(IndexT, Vec<IndexT>)> {
+        let nonempty_queues = self.insertion_queues
+            .par_iter_mut()
+            .enumerate()
+            .filter_map(|(i, queue)| {
+                let mut queue = queue.lock().unwrap();
+                if !queue.is_empty() {
+                    let queue_copy = queue.clone();
+                    queue.clear();
+                    Some((i as IndexT, queue_copy))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+        nonempty_queues
     }
 }
 
@@ -78,3 +144,5 @@ impl From<&VectorGraph> for ClassicGraph {
         output_graph
     }
 }
+
+unsafe impl Sync for VectorGraph {}
