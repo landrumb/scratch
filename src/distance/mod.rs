cc332ece2b9cb129798a2b87463ad0e1cfb77813
Ben Landrum <8enlandrum@gmail.com>
2025-05-04 15:59:50 -0400

    squared euclidean

--- src/distance/mod.rs
+++ src/distance/mod.rs
@@ -4,6 +4,7 @@ pub use self::euclidean_::{euclidean, sq_euclidean};
 
 pub enum Distance {
     Euclidean,
+    SquaredEuclidean,
     Cosine,
     InnerProduct,
 }
@@ -11,6 +12,7 @@ impl Distance {
     pub fn from_str(s: &str) -> Self {
         match s {
             "euclidean" => Distance::Euclidean,
+            "squared_euclidean" => Distance::SquaredEuclidean,
             "cosine" => Distance::Cosine,
             "inner_product" => Distance::InnerProduct,
             _ => panic!("Unknown distance: {}", s),
