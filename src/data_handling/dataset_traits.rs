347a01d76621e89d05562261e9c74dc4d212d5fe
Ben Landrum <8enlandrum@gmail.com>
2025-05-04 17:09:13 -0400

    reordered dataset traits

--- src/data_handling/dataset_traits.rs
+++ src/data_handling/dataset_traits.rs
@@ -2,6 +2,15 @@
 
 use std::ops::Sub;
 
+
+pub trait Numeric: Copy + Into<f64> + Sub<Output = Self> + Default + Send + Sync {}
+
+impl Numeric for f32 {}
+impl Numeric for f64 {}
+impl Numeric for i32 {}
+impl Numeric for i8 {}
+
+
 pub trait Dataset<T>: Send + Sync {
     fn compare_internal(&self, i: usize, j: usize) -> f64; // this probably should take reference args(?)
     fn compare(&self, q: &[T], i: usize) -> f64;
@@ -45,12 +54,3 @@ pub trait Dataset<T>: Send + Sync {
         result.into_boxed_slice()
     }
 }
-
-
-
-pub trait Numeric: Copy + Into<f64> + Sub<Output = Self> + Default + Send + Sync {}
-
-impl Numeric for f32 {}
-impl Numeric for f64 {}
-impl Numeric for i32 {}
-impl Numeric for i8 {}
