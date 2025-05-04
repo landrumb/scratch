db034997e519e06e672078bde3e63b191af2318a
Ben Landrum <8enlandrum@gmail.com>
2025-05-04 16:23:45 -0400

    much better approach to dealing with subsets

--- src/bin/build_slow_preprocessing.rs
+++ src/bin/build_slow_preprocessing.rs
@@ -8,7 +8,7 @@ use scratch::constructions::slow_preprocessing::build_global_local_graph;
 use scratch::constructions::neighbor_selection::{incremental_greedy, PairwiseDistancesHandler};
 use scratch::data_handling::dataset::{Subset, VectorDataset};
 use scratch::data_handling::dataset_traits::Dataset;
-use scratch::data_handling::fbin::read_fbin;
+use scratch::data_handling::fbin::{read_fbin, read_fbin_subset};
 use scratch::graph::{beam_search, ClassicGraph, Graph, IndexT};
 use scratch::util::ground_truth::compute_ground_truth;
 use scratch::util::recall::recall;
@@ -39,16 +39,16 @@ fn main() {
     let mut start = Instant::now();
     // let dataset: VectorDataset<f32> = read_fbin(data_path);
     
-    let boxed_dataset: Box<VectorDataset<f32>> = Box::new(read_fbin(data_path));
-    // let mut subset_size: usize = boxed_dataset.size();
-    let subset_size: usize = std::env::var("SUBSET_SIZE")
-        .unwrap_or_else(|_| boxed_dataset.size().to_string())
-        .parse()
-        .unwrap_or(boxed_dataset.size());
+    let dataset: VectorDataset<f32>;
 
-    println!("Using subset of size {}", subset_size);
-    let subset_indices = (0..subset_size).collect::<Vec<usize>>();
-    let subset = Subset::new(boxed_dataset, subset_indices);
+    if let Ok(subset_size_str) = std::env::var("SUBSET_SIZE") {
+         dataset = read_fbin_subset(data_path, subset_size_str.parse().unwrap());
+        println!("Using subset of size {}", subset_size_str);
+    } else {
+        dataset = read_fbin(data_path);
+        println!("Using full dataset of size {}", dataset.size());
+    }
+
     
     let elapsed = start.elapsed();
     println!(
@@ -65,10 +65,10 @@ fn main() {
     // let graph = build_slow_preprocesssing(&dataset, 1.01);
 
 
-    let nested_boxed_distances = (0..subset.size())
+    let nested_boxed_distances = (0..dataset.size())
         .into_par_iter()
         .map(|i| {
-            subset.brute_force_internal(i)
+            dataset.brute_force_internal(i)
                 .iter()
                 .map(|(j, dist)| (*j as IndexT, *dist))
                 .collect::<Box<[(IndexT, f32)]>>()
@@ -82,8 +82,8 @@ fn main() {
     //     robust_prune_unbounded(candidates.iter().map(|i| (*i, subset.compare_internal(*i as usize, center as usize).to_f32().unwrap())).collect(), 1.01, &subset)
     // });
 
-    let graph = build_global_local_graph(&subset, |center, candidates| {
-        incremental_greedy(center, candidates, &subset, 1.0, &pairwise_distances, None)
+    let graph = build_global_local_graph(&dataset, |center, candidates| {
+        incremental_greedy(center, candidates, &dataset, 1.0, &pairwise_distances, None)
     });
     // let graph = build_global_local_graph(&subset, |center, candidates| {
     //     naive_semi_greedy_prune(center, candidates, &subset, 1.01, &pairwise_distances)
@@ -96,7 +96,7 @@ fn main() {
     );
 
     println!("Total edges: {}", graph.total_edges());
-    println!("Average degree: {}", graph.total_edges() / subset.size());
+    println!("Average degree: {}", graph.total_edges() / dataset.size());
     println!("Max degree: {}", graph.max_degree());
 
     // Load queries
@@ -107,7 +107,7 @@ fn main() {
     let results: Vec<Vec<u32>> = (0..queries.size())
     // let results: Vec<Vec<u32>> = (0..subset_size)
         .into_par_iter()
-        .map(|i| beam_search(queries.get(i), &graph, &subset, 0, 20, None))
+        .map(|i| beam_search(queries.get(i), &graph, &dataset, 0, 20, None))
         .collect();
 
     let elapsed = start.elapsed();
@@ -123,7 +123,7 @@ fn main() {
     // let gt = GroundTruth::read(gt_path);
 
     // compute gt over the subset
-    let gt = compute_ground_truth(&queries, &subset, 10).unwrap();
+    let gt = compute_ground_truth(&queries, &dataset, 10).unwrap();
 
     let graph_recall = (0..results.len())
         .map(|i| recall(results[i].as_slice(), gt.get_neighbors(i)))
@@ -137,12 +137,12 @@ fn main() {
     println!("saved graph to disk");
 
     // do the same querying with the dataset itself
-    let dataset_results: Vec<Vec<u32>> = (0..subset_size)
+    let dataset_results: Vec<Vec<u32>> = (0..dataset.size())
         .into_par_iter()
-        .map(|i| beam_search(subset.get(i), &graph, &subset, 0, 1, None))
+        .map(|i| beam_search(dataset.get(i), &graph, &dataset, 0, 1, None))
         .collect();
 
-    let internal_gt = compute_ground_truth(&subset, &subset, 2).unwrap();
+    let internal_gt = compute_ground_truth(&dataset, &dataset, 2).unwrap();
 
     let dataset_recall = (0..dataset_results.len())
         .map(|i| recall(dataset_results[i].as_slice(), &internal_gt.get_neighbors(i)[..1]))
@@ -151,7 +151,7 @@ fn main() {
 
     println!("self recall: {:.5}", dataset_recall);
 
-    let n_points_connected_to_nearest_neighbor = (0..subset.size())
+    let n_points_connected_to_nearest_neighbor = (0..dataset.size())
         .into_par_iter()
         .filter(|i| {
             let neighbors = graph.neighbors(*i as IndexT);
@@ -162,10 +162,10 @@ fn main() {
         .count()
         ;
 
-    println!("Fraction of points connected to their nearest neighbor: {:.5}", n_points_connected_to_nearest_neighbor as f64 / subset.size() as f64);
+    println!("Fraction of points connected to their nearest neighbor: {:.5}", n_points_connected_to_nearest_neighbor as f64 / dataset.size() as f64);
 
     // how many points have an incoming edge from their nearest neighbor (should be all of them)
-    let n_points_connected_to_inverse_nn = (0..subset.size())
+    let n_points_connected_to_inverse_nn = (0..dataset.size())
         .into_par_iter()
         .filter(|i| {
             let nearest_neighbor = internal_gt.get_neighbors(*i)[1];
@@ -174,7 +174,7 @@ fn main() {
         }
         )
         .count();
-    println!("Fraction of points with an incoming edge from their nearest neighbor: {:.5}", n_points_connected_to_inverse_nn as f64 / subset.size() as f64);
+    println!("Fraction of points with an incoming edge from their nearest neighbor: {:.5}", n_points_connected_to_inverse_nn as f64 / dataset.size() as f64);
 
      // print the results of the first 10 queries in the dataset
     // for i in 0..10 {
--- src/data_handling/fbin.rs
+++ src/data_handling/fbin.rs
@@ -2,7 +2,7 @@
 
 use std::fs::File;
 use std::io::{BufReader, Read};
-use std::path::Path;
+use std::path::{self, Path};
 
 use memmap2::Mmap;
 
@@ -31,6 +31,44 @@ pub fn read_fbin<T: Numeric>(path: &Path) -> VectorDataset<T> {
         expected_size + 8,
         mmap.len()
     );
+
+    let data = {
+        let mut data = vec![T::default(); n as usize * dim as usize];
+        reader
+            .read_exact(unsafe {
+                std::slice::from_raw_parts_mut(data.as_mut_ptr() as *mut u8, expected_size)
+            })
+            .expect("could not read data");
+        Box::from(data)
+    };
+
+    VectorDataset::new(data, n as usize, dim as usize)
+}
+
+/// reads only a subset of the dataset from an fbin file, but is otherwise the same as `read_fbin`
+pub fn read_fbin_subset<T: Numeric>(path: &Path, subset_size: usize) -> VectorDataset<T> {
+    let file = File::open(path).expect("could not open file");
+    let mmap = unsafe { Mmap::map(&file).expect("could not mmap file") };
+    let mut reader = BufReader::new(&*mmap);
+
+    let mut n_buf = [0u8; 4];
+    reader.read_exact(&mut n_buf).expect("could not read n");
+    let n = u32::from_le_bytes(n_buf);
+
+    assert!(
+        subset_size <= n as usize,
+        "subset size {} is larger than dataset size {}",
+        subset_size,
+        n
+    );
+
+    let n = subset_size as u32;
+
+    let mut dim_buf = [0u8; 4];
+    reader.read_exact(&mut dim_buf).expect("could not read dim");
+    let dim = u32::from_le_bytes(dim_buf);
+    let element_size = std::mem::size_of::<T>();
+    let expected_size = subset_size * dim as usize * element_size;
 
     let data = {
         let mut data = vec![T::default(); n as usize * dim as usize];
