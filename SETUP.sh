55fb6df71925680495b4d75b0eb30e3ec7b86130
Ben Landrum <ben.l@pinecone.io>
2025-07-17 14:46:29 -0400

    updated setup script

--- SETUP.sh
+++ SETUP.sh
@@ -3,17 +3,17 @@
 # this script unpacks the data files and computes groundtruth for the dataset
 
 # unzipping data.tar.gz
-if [ -f "data.tar.gz" ]; then
-    echo "Unpacking data.tar.gz..."
-    tar -xzf data.tar.gz
+if [ -f "word2vec-google-news-300_50000_lowercase.tar.gz" ]; then
+    echo "Unpacking word2vec-google-news-300_50000_lowercase.tar.gz..."
+    tar -xzf word2vec-google-news-300_50000_lowercase.tar.gz
 else
-    echo "data.tar.gz not found. Please ensure it is in the current directory."
+    echo "word2vec-google-news-300_50000_lowercase.tar.gz not found. Please ensure it is in the current directory."
     exit 1
 fi
 
-# for every subdir of data, compute groundtruth
+# for every subdir of data, compute groundtruth if it doesn't already exist
 for dir in data/*/; do
-    if [ -d "$dir" ]; then
+    if [ -d "$dir" ] && [ ! -f "${dir}GT" ]; then
         echo "Computing groundtruth for $dir..."
         cargo run -r --bin compute_groundtruth -- -d ${dir}base.fbin -o ${dir}GT -q ${dir}query.fbin
         if [ $? -ne 0 ]; then
